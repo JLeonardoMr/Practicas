ae2214a48f62739a5cfeb3126082e062aac62efe
JLeonardoMr <jhondrilmr@gmail.com>
2021-12-15 05:23:18 -0400

    asdas
    asdas

--- ejercicioajax2/js/ejercicio.js
+++ ejercicioajax2/js/ejercicio.js
@@ -17,12 +17,13 @@ const $FILES = document.getElementById('files');
 
 const UPLOADER = (file) =>{
     console.log(file);
-    const XHR = new XMLHttpRequest(),
-        formData = new FormData();
+    const XHR = new XMLHttpRequest();
+    let formData = new FormData();
     //este beta recibe todo como un formulario, esto quiere decir que cada archivo que suba quedara registrado como parte de un formulario
+    console.log(formData.get('file'));
     formData.append('file',file);
-    console.log(formData);
-
+    console.log(formData.get('file'));
+    
     XHR.addEventListener('readystatechange',e=>{
         if (XHR.readyState !== 4) return;
         if (XHR.status >= 200 && XHR.statusText < 300) {
@@ -33,7 +34,7 @@ const UPLOADER = (file) =>{
         }
     })
 
-    XHR.open("POST","/module/uploader.php");
+    XHR.open("POST","uploader.php");
     XHR.setRequestHeader('enc-type','multipart/form-data');
     XHR.send(formData);
 }
